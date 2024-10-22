5a3d864b810b1ae59fa5c733b9e98c7d91724fef
maite <maite.sanchez@gm2dev.com>
2024-10-22 12:28:51 -0300

    feat(streams): add exercises

--- file-system/fs.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-fs.readFile('files/data.txt', 'utf8', (err, data) => {
-    if(err){
-        console.error('Could not read file:', err);
-        return;
-    }
-    console.log(data);
-})
