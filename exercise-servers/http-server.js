a50aeabf9bafc80055b27b40d701131b0b5b925c
maite <maite.sanchez@gm2dev.com>
2024-10-24 00:53:01 -0300

    fix(http-servers): change listening message

--- exercise-servers/http-server.js
+++ exercise-servers/http-server.js
@@ -19,5 +19,5 @@ const server = http.createServer((req, res) => {
 const port = 3000;
 
 server.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
